ed1408e5b962f1de5c85b62c07d38f9f91ad4dac
luke.moon <gathering11@naver.com>
2020-08-10 01:08:31 +0900

    최초 기능

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const data = [];
+
+let genId = 1;
+const idGenerator = () => {
+  return genId++;
+};
+
+router.get("/", function (_, res) {
+  res.json(data);
+});
+
+router.post("/", function (req, res) {
+  if (!req.body.result) return res.status(400).send({ error: "bad request" });
+  data.push({
+    id: idGenerator(),
+    result: req.body.result,
+  });
+  res.json("ok");
+});
+
+router.delete("/:id", function (req, res) {
+  const paramId = req.params.id;
+  if (!paramId) {
+    return res.status(400).send({ error: "bad request" });
+  }
+  const idx = data.findIndex((x) => Number(x.id) === Number(paramId));
+  if (idx > -1) {
+    data.splice(idx, 1);
+    return res.send("ok");
+  } else {
+    return res.send("no data");
+  }
 });
 
 module.exports = router;
